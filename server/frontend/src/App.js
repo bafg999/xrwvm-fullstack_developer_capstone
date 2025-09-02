5b2ca35a7ccf07dfe33f1f8e7949444504c0e026
bafg999 <bflorez@unimayor.edu.co>
2025-09-02 17:00:02 +0000

    ajustes vistas y urls

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/registration/register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/registration" element={<LoginPanel />} />
     </Routes>
   );
 }
