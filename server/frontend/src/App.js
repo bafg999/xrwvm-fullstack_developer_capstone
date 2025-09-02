d8a8e000655cc64667d78bb4f54afd1db6d9870c
bafg999 <bflorez@unimayor.edu.co>
2025-09-02 17:13:54 +0000

    ajustes app.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,12 @@
 import LoginPanel from "./components/Login/Login"
-import LoginPanel from "./components/registration/register"
+import Register from "./components/Register/Register"; 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/registration" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
